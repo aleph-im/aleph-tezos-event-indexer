1763e48c176ad5c0a62867842363da968879f369
aliel <ali.elb@gmail.com>
2022-05-26 13:55:46 +0200

    add event type filter

--- src/graphql/server.py
+++ src/graphql/server.py
@@ -14,11 +14,21 @@ class Query(graphene.ObjectType):
     events = graphene.List(types.Events, limit=graphene.Int(default_value=100, description="max value = 1000"),
                            reverse=graphene.Boolean(default_value=True),
                            skip=graphene.Int(default_value=0),
+                           **{"type": graphene.String(default_value=None, description="Event type, ex: burn_event")},
                            source=graphene.String(default_value=None, description="Source address"),
                            destination=graphene.String(default_value=None, description="Destination address"),
                            operation_hash=graphene.String(default_value=None, description="Operation hash"),
                            block_hash=graphene.String(default_value=None, description="Block hash"))
-    async def resolve_events(self, info, limit, reverse, skip, source, destination, operation_hash, block_hash):
+    async def resolve_events(self, info, **kwargs):
+        limit = kwargs["limit"]
+        reverse = kwargs["reverse"]
+        skip = kwargs["skip"]
+        target_type = kwargs["type"]
+        source = kwargs["source"]
+        destination = kwargs["destination"]
+        operation_hash = kwargs["operation_hash"]
+        block_hash = kwargs["block_hash"]
+        
         max_limit = 1000
         if limit > max_limit:
             limit = max_limit
@@ -31,7 +41,7 @@ class Query(graphene.ObjectType):
             address = block_hash or operation_hash or source or destination
 
         events = eventStorage.get_events(reverse=reverse, limit=limit, skip=skip, index_address=address)
-        if index_list_len < 2:
+        if index_list_len < 2 and target_type is None:
             return events
 
         # filter if more than one criteria are provided
@@ -47,6 +57,9 @@ class Query(graphene.ObjectType):
                 idx_to_delete.append(idx)
                 continue
             if block_hash is not None and block_hash != event["block_hash"]:
+                idx_to_delete.append(idx)
+                continue
+            if target_type is not None and target_type != event["type"]:
                 idx_to_delete.append(idx)
                 continue
 
